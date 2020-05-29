adcb6a4e666418d8903f51b0f832eff22129c16e
AnstettVictor <anstett.victor67@gmail.com>
2020-05-29 17:48:17 +0200

    (6/10) Ecran d'acceuil

--- game.py
+++ game.py
@@ -5,17 +5,54 @@ from monster import Monster
 class Game:
 
     def __init__(self):
+        # definir si notre jeu a commencé ou non
+        self.is_playing = False
         #generer notre joueur
         self.all_players = pygame.sprite.Group()
         self.player = Player(self)
         self.all_players.add(self.player)
-
-
         #groupe de monstre
         self.all_monsters = pygame.sprite.Group()
         self.pressed = {}
+
+
+    def start(self):
+        self.is_playing = True
         self.spawn_monster()
         self.spawn_monster()
+    def game_over(self):
+        #remettre le jeu a neuf, retirer les monstres , remettre le joueur a 100 de vie, jeu en attente
+        self.all_monsters = pygame.sprite.Group()
+        self.player.health = self.player.max_health
+        self.is_playing = False
+
+    def update(self, screen):
+         # appliquer l'image de monjoueur
+         screen.blit(self.player.image, self.player.rect)
+
+         # actualiser la barre de vie du joueur
+         self.player.update_health_bar(screen)
+
+         # recupere les projectiles dui joueur
+         for projectile in self.player.all_projectiles:
+             projectile.move()
+
+         # recuperer les monstres de notre jeu
+         for monster in self.all_monsters:
+             monster.forward()
+             monster.update_health_bar(screen)
+
+         # appliquer l'ensemble des images de mon groupe de projectile
+         self.player.all_projectiles.draw(screen)
+
+         # appliquer l'ensemble des images de mon groupes de monstres
+         self.all_monsters.draw(screen)
+
+         # verifier si le joueur souhaite aller a fauche ou a droite
+         if self.pressed.get(pygame.K_RIGHT) and self.player.rect.x + self.player.rect.width < screen.get_width():
+             self.player.move_right()
+         elif self.pressed.get(pygame.K_LEFT) and self.player.rect.x > 0:
+            self.player.move_left()
 
     def check_collision(self, sprite, group):
         return pygame.sprite.spritecollide(sprite, group, False, pygame.sprite.collide_mask)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 from game import Game
 pygame.init()
@@ -11,9 +12,22 @@ screen = pygame.display.set_mode((1080, 720))
 # importer le background
 background = pygame.image.load('assets/bg.jpg')
 
+# importer la banniere
+banner = pygame.image.load('assets/banner.png')
+banner = pygame.transform.scale(banner, (500, 500))
+banner_rect = banner.get_rect()
+banner_rect.x = math.ceil(screen.get_width() / 4)
+
+#importer le bouton pour lancer la partie
+play_button = pygame.image.load('assets/button.png')
+play_button = pygame.transform.scale(play_button, (400, 150))
+play_button_rect = play_button.get_rect()
+play_button_rect.x = math.ceil(screen.get_width() / 3.33)
+play_button_rect.y = math.ceil(screen.get_height() / 2)
 #charger notre jeu
 
 game = Game()
+
 
 running = True
 
@@ -23,32 +37,17 @@ while running:
     #appliquer l'arriere plan du jeu
     screen.blit(background, (0, -200))
 
-    #appliquer l'image de monjoueur
-    screen.blit(game.player.image, game.player.rect)
+    # verifier si notre jeu a commencé ou non
+    if game.is_playing:
+        # delcencher les instcrutions de la partie
+        game.update(screen)
+    #verifier si notre jeu n'a pas commencé
+    else:
+        # ajouter mon ecran de bienvienue
+        screen.blit(play_button, play_button_rect)
+        screen.blit(banner, banner_rect)
 
-    #actualiser la barre de vie du joueur
-    game.player.update_health_bar(screen)
 
-    #recupere les projectiles dui joueur
-    for projectile in game.player.all_projectiles:
-        projectile.move()
-
-    # recuperer les monstres de notre jeu
-    for monster in game.all_monsters:
-        monster.forward()
-        monster.update_health_bar(screen)
-
-    # appliquer l'ensemble des images de mon groupe de projectile
-    game.player.all_projectiles.draw(screen)
-
-    # appliquer l'ensemble des images de mon groupes de monstres
-    game.all_monsters.draw(screen)
-
-    #verifier si le joueur souhaite aller a fauche ou a droite
-    if game.pressed.get(pygame.K_RIGHT) and game.player.rect.x + game.player.rect.width < screen.get_width():
-        game.player.move_right()
-    elif game.pressed.get(pygame.K_LEFT) and game.player.rect.x > 0:
-        game.player.move_left()
     #mettre a jour l'ecran
     pygame.display.flip()
 
@@ -68,4 +67,10 @@ while running:
 
         elif event.type == pygame.KEYUP:
             game.pressed[event.key] = False
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            #verification pour savoir si la souris est en collision avec le bouton jouer
+            if play_button_rect.collidepoint(event.pos):
+                #mettre le jeu en mode "lancé"
+                game.start()
 
